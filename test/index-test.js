080bebb6cbcb3221c370183f73ff9d19bf390f90
mamadou.m.diakhate@gmail.com <mamadou.m.diakhate@gmail.com>
2018-06-18 05:23:59 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,11 +1,12 @@
 /*global describe, it */
 
-describe('Fix the Scope', function() {
+/*describe('Fix the Scope', function() {
   describe('myAnimal()', function() {
     it('returns my animal', () => {
       expect(window.myAnimal()).toEqual('dog')
     })
-  })
+  })*/
+  
 
   describe('yourAnimal()', function() {
     it('returns your animal', function(){
